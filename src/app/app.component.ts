cd66be3f51218ee913c94aa30324e133f073c94d
majes <liones2014@mail.ru>
2020-11-24 17:56:20 +0700

    search-filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Post {
+  title: string
+  text: string
+
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'pipes';
+  e: number = Math.E
+  str = 'Hi World'
+  float = 0.45
+  obj = {
+    a: {a: 1},
+    c: 3
+  }
+  posts = [
+    {title: 'Beer', text: 'Самое лучшие пивов мире'},
+    {title: 'Bread', text: 'Самое лучшие Bread мире'},
+    {title: 'Jsz', text: 'Самое лучшие Jsz мире'},
+  ]
+  search = ''
+  searchField = 'title'
 }
